d9caffc0273e140a060c233d21d553d494f4bcb4
A.C.E07 <a.c.e8009aiden@gmail.com>
2024-06-15 20:52:08 +0800

    Removed `mpo`
    - Removed `mpo` for the time being.
    - Added a method for defining an `mps` representation of a unitary operator.

--- qmprs/__init__.py
+++ qmprs/__init__.py
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__all__ = ["mpo",
-           "mps",
+__all__ = ["mps",
            "synthesis",]
 
-from qmprs import mpo, mps, synthesis
+from qmprs import mps, synthesis
